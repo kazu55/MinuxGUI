cf67f4da09a268c90e8a1f71da62055aa5b3f63f
たまさん <58621773+kazu55@users.noreply.github.com>
2021-03-26 16:23:09 +0900

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -26,41 +26,8 @@ term.write("I")
 sleep(2)
 term.clear()
 
-i = 0
-
-repeat
-  term.setTextColor(colors.white)
-  term.setCursorPos(25,10)
-  term.write("|")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("/")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("/")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("-")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("-")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("\\")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("\\")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  term.write("|")
-  term.setCursorPos(25,10)
-  sleep(0.2)
-  i = i + 1
-until i == 5
-
 shell.run("1")
 
-if i == 5 then
   term.setBackgroundColor(colors.blue)
   term.clear()
   sleep(0.3)
