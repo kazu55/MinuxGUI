10645e35d44d67f2c2f2ec70d3f5f43e57329ece
たまさん <58621773+kazu55@users.noreply.github.com>
2021-04-01 10:07:14 +0900

    Update shutdown1.lua

--- shutdown1.lua
+++ shutdown1.lua
@@ -1,7 +1,45 @@
-term.setBackgroundColor(colors.black)
-term.setCursorPos(1,1)
-term.clear()
-print("Did you save before shutting down?")
-print("After saving, press the enter key.")
-read()
-os.shutdown()
+button = { }
+
+function button:create(x,y,w,h,color1,color2,text, onclick)
+  button.x = x
+  button.y = y
+  button.w = w
+  button.h = h
+  button.color1 = color1
+  button.color2 = color2
+  button.text = text
+  button.onClick = onClick
+  
+  --create button window
+  term.setCursorPos(x,y)
+  button.window_ = window.create(term.current(),x,y,w,h)
+  button.window_.setBackgroundColor(button.color1)
+  button.window_.clear()
+  
+  --add text
+  newX = math.floor((button.w - #button.text)/2) + 1
+  newY = math.floor(button.h/2) + 1
+  button.window_.setCursorPos(newX, newY)
+  button.window_.setTextColor(color2)
+  button.window_.write(text)
+  button.window_.redraw()
+ 
+end
+
+
+function button:getClick(x,y)
+
+	print("test")
+  
+end
+  
+--buttons
+buttons = {}
+buttons.test1 = button:create(20,8,10,3,colors.red,colors.black,"Shutdown")
+
+while true do
+  
+  event, side, x, y = os.pullEvent("mouse_click")
+  os.shutdown()
+  
+end
