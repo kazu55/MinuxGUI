bdee5e02dd993803b29a0579dc6beeece572bd58
たまさん <58621773+kazu55@users.noreply.github.com>
2021-04-03 16:32:50 +0900

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,8 +1,8 @@
 term.clear()
 term.setCursorPos(1,1)
 sleep(2)
-print("Press any key here to open the boot menu. Wait 10 seconds.")
-local myTimer = os.startTimer(10)
+print("Press any key here to open the boot menu")
+local myTimer = os.startTimer(2)
 
 while true do
   local event, par1 = os.pullEvent()
@@ -10,7 +10,7 @@ while true do
   if event == "timer" and par1 == myTimer then
     break
   elseif event == "key" then
-    print("Start BootMenu...")
+    print("Start BootSelector...")
     term.setBackgroundColor(colors.black)
     sleep(1.261)
     term.clear()
