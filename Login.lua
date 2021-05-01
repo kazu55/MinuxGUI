7e1a44d1f3da482b6e57bf0926edb2763ef9244e
たまさん <58621773+kazu55@users.noreply.github.com>
2021-05-01 11:49:02 +0900

    Update Login.lua

--- Login.lua
+++ Login.lua
@@ -1,10 +1,32 @@
 Key = "u6ygvxErMyfk"
+pass = "pass"
+
+local function centerText(text)
+  local x,y = term.getSize()
+  local x2,y2 = term.getCursorPos()
+  term.setCursorPos(math.ceil((x / 2) - (text:len() / 2)), y2)
+  write(text)
+end
+
+term.clear()
+term.setBackgroundColor(colors.black)
 
 if fs.exists("key.txt") == true then
-  shell.run("Desktop")
+  local image = paintutils.loadImage("themes/Sakura.nfp")
+  paintutils.drawImage(image, 1, 1)
+  term.setCursorPos(1,18)
+  term.setTextColor(colors.red)
+  print("Enter Password.")
+  b = read()
+  if b == pass then
+    shell.run("Desktop")
+  end
 end
-print("Please write the key")
-a = read()
+
+if fs.exists("key.txt") == false then
+  print("Please write the key")
+  a = read()
+end
 
 if a == Key then
   local h = fs.open("key.txt", "w")
@@ -12,5 +34,7 @@ if a == Key then
   h.close()
   shell.run("Desktop")
 else
+  term.clear()
+  term.setBackgroundColor(colors.black)
   shell.run("Login")
 end
